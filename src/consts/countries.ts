e8cf0226b61b1324b764fdc0ff148d0f195443e8
Miguel Amores <migueamores@gmail.com>
2024-04-17 21:39:05 -0500

    create boxers and countries consts

---
+++ src/consts/countries.ts
@@ -0,0 +1,17 @@
+export const COUNTRIES: Record<string, { name: string }> = {
+  mx: {
+    name: "Mexico",
+  },
+  us: {
+    name: "United States",
+  },
+  br: {
+    name: "Brazil",
+  },
+  ca: {
+    name: "Canada",
+  },
+  es: {
+    name: "Spain",
+  },
+} as const;
